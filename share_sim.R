4376d8b54f1aff142b3a1d663ef0cec35554fba5
Jenna Krall <jkrall@jhsph.edu>
2014-07-24 14:05:28 -0400

    Fix numeric seed, call to reps`

--- share_sim.R
+++ share_sim.R
@@ -3,7 +3,7 @@
 
 rm(list = ls())
 args <- commandArgs(TRUE)
-seed <- args[[1]]
+seed <- as.numeric(args[[1]])
 print(seed)
 
 ########
@@ -42,7 +42,7 @@ keeps1 <- keeps
 uneqs = NULL
 day1 <- NULL
     
-}else if(seed == 5) {
+if(seed == 5) {
     uneqs <- c(12, 15, 16, 20)
 }else if(seed == 6) {
     day1 <- rep(c(200, 500, 1000, 1000, 5000), each = 5)
@@ -57,7 +57,7 @@ set.seed(seeds1[seed])
 
 
 simout <- multsims(nsims = ns, names = names, 
-    nmons = mon, regs = reg, ndays = nd, 
+    nmons = mon, reps = reg, ndays = nd, 
     PCs = vec, keeps = keeps1, 
     cms = cms, sds = sds, 
     unequal = uneqs, days = day1, cut = 1, thres = pi/4)	
