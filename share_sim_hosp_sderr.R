19062c973e17deb918a18f6f16d9b344dca65d9f
Jenna Krall <jenna.krall@gmail.com>
2014-12-29 14:16:52 -0500

    Initial commit for changing sderr

---
+++ share_sim_hosp_sderr.R
@@ -0,0 +1,73 @@
+### File to simulate mortality effects
+rm(list = ls())
+args <- commandArgs(TRUE)
+sim <- as.numeric(args[[1]])
+err <- as.numeric(args[[2]])
+print(c(sim, err))
+
+
+
+
+
+
+#load others
+library(share)
+library(handles)
+library(sharesim)
+
+
+
+
+
+
+#load data
+data(keeps, cms, sds, vec, names)
+
+# new combo of sources
+load("~/SHARE/sharesim/data_share_sim_revisedcombo.RData")
+
+
+#set up info
+nmons <- 25
+ndays <- 500
+etas <- c(3, 1, 0.75, 0.5, 1, 1, 2)
+seeds <- c(9763, 398)
+reps1 <- c(25, 5)
+
+
+sderrs <- c(0.001, 0.1, 0.5)
+sderr1 <- sderrs[err]
+
+###############
+reps <- reps1[sim]
+outmult <- list()
+set.seed(seeds[sim])
+for(i in 1 : 100) {
+    print(i)
+    outmult[[i]] <- outerSIMhosp(names, nmons, reps, ndays, vec, keeps, 
+                                  cms, sds, etas, sderr = sderr1)
+}
+
+
+
+
+regcoef <- sapply(outmult, function(x) x[[1]], simplify = F)
+percinc1 <- sapply(outmult, function(x) x[[2]], simplify = F)
+iqrs <- apply(sapply(outmult, function(x) x[[3]]), 1, median)
+outrest <- gethospsim(regcoef, iqrs)[[2]]
+mse <- msefun(percinc1, etas, rownames(outmult[[1]][[1]][[1]]))
+out <- outmult[[1]][[2]]
+
+
+
+
+
+
+
+
+# save output
+sims <- c("A", "B")
+save(out, outmult, mse, outrest, iqrs, percinc1, regcoef,
+     file = paste0(sims[sim], "simhosp_multres_sderr", err, ".RData"))
+
+
---
+++ share_sim_sderr.R
@@ -0,0 +1,65 @@
+# File to run simulation study for SHARE
+# No health, just source identification
+
+rm(list = ls())
+args <- commandArgs(TRUE)
+seed <- as.numeric(args[[1]])
+print(seed)
+
+########
+#load packages (make sure recent versions are installed)
+library(devtools)
+library(handles)
+library(share)
+library(sharesim)
+
+########
+
+
+
+
+
+
+#######
+#######
+#######
+#######
+# Simulate data
+#######
+#######
+#######
+#######
+
+#universal values
+ns <- 100
+nd <- 1000
+seeds1 <- c(4658, 4260, 8656, 8745)
+mon <- 25
+reg <- 5
+
+#set params for simulation
+keeps1 <- keeps
+uneqs <- NULL
+day1 <- NULL
+    
+    
+set.seed(seeds1[seed])
+
+sderrs <- c(0.001, 0.1, 0.5)
+sderr1 <- sderrs[seed]
+
+simout <- multsims(nsims = ns, names = names, 
+    nmons = mon, reps = reg, ndays = nd, 
+    PCs = vec, keeps = keeps1, 
+    cms = cms, sds = sds, 
+    unequal = uneqs, days = day1, cut = 1, thres = pi/4, prnt = T,
+    sderr = sderr1)	
+
+
+save(simout, file = paste0("simout_sderr_", seed, ".RData"))
+
+
+
+
+
+
